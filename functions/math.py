0a5d798394b35809a0a29f35509324ea580214f5
Alice <alicemoraaongongo@gmail.com>
2023-03-31 15:35:18 +0300

    python functions

---
+++ functions/math.py
@@ -0,0 +1,16 @@
+def addition(a,b):
+    answers = (a+b)
+    return answers
+    
+def subtraction (a,b):
+    answers = (a-b)
+    return answers
+
+def multiplication(a,b):
+    answers = (a*b)
+    return answers
+
+def modulus(a,b):
+    answers = (a%b)
+    return answers    
+        
